9c5ec6af60fa22cd7d859079bb1ce77366acf9cd
mandyche <mandyche@umich.edu>
2024-12-16 14:48:17 -0500

    added photos and final meals

--- cookbook/src/components/RecipeCard.js
+++ cookbook/src/components/RecipeCard.js
@@ -5,7 +5,7 @@ function RecipeCard({ recipe }) {
     return (
         <div>
             <h2>{recipe.id}. {recipe.title}</h2>
-            <img src={`${process.env.PUBLIC_URL}/${recipe.image_path}`} alt={`${recipe.id} image.`} className='recipe-card-img'/>
+            <img src={`${process.env.PUBLIC_URL}/${recipe.folder}/${recipe.image_path[0]}`} alt={`${recipe.id} image.`} className='recipe-card-img'/>
             <p>{recipe.description}</p>
             <Link to={`/recipe/${recipe.id}`} className="button-link">
                 View Recipe
--- cookbook/src/pages/RecipeDetail.js
+++ cookbook/src/pages/RecipeDetail.js
@@ -9,6 +9,7 @@ function RecipeDetail() {
     const storageKey = `recipe-note-${id}`;
     const [note, setNote] = useState('');
     const [isEditing, setIsEditing] = useState(false);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     useEffect(() => {
         const savedNote = localStorage.getItem(storageKey);
@@ -39,14 +40,63 @@ function RecipeDetail() {
         return <div>Recipe not found.</div>;
     }
 
+    const imageCount = recipe.image_path.length;
+
+    const handleNext = () => {
+        setCurrentImageIndex((currentImageIndex + 1) % imageCount);
+    };
+
+    const handlePrev = () => {
+        setCurrentImageIndex(
+            (currentImageIndex - 1 + imageCount) % imageCount
+        );
+    };
+
     return (
         <div>
             <h1>{recipe.id}. {recipe.title}</h1>
-            <img 
-                src={`${process.env.PUBLIC_URL}/${recipe.image_path}`} 
+            {/* <img 
+                src={`${process.env.PUBLIC_URL}/${recipe.image_path[0]}`} 
                 alt={`${recipe.id} depiction`} 
                 className="recipe-detail-img" 
-            />
+            /> */}
+            <div className="carousel-container" style={{ position: 'relative', textAlign: 'center' }}>
+                <img
+                    src={`${process.env.PUBLIC_URL}/${recipe.folder}/${recipe.image_path[currentImageIndex]}`}
+                    alt={`${recipe.id} depiction ${currentImageIndex + 1}`}
+                    className="recipe-detail-img"
+                    style={{ width: '100%', maxHeight: '500px', objectFit: 'cover', borderRadius: '10px' }}
+                />
+
+                <button
+                    onClick={handlePrev}
+                    className='carousel-button'
+                    style={{
+                        left: '1em'
+                    }}
+                >
+                    ◀
+                </button>
+                <button
+                    onClick={handleNext}
+                    className='carousel-button'
+                    style={{
+                        right: '1em'
+                    }}
+                >
+                    ▶
+                </button>
+
+                <div style={{ marginTop: '10px' }}>
+                    {recipe.image_path.map((_, index) => (
+                        <span
+                            key={index}
+                            onClick={() => setCurrentImageIndex(index)}
+                            className={`carousel-dot ${currentImageIndex === index ? 'active' : ''}`}
+                        ></span>
+                    ))}
+                </div>
+            </div>
             <h3>Ingredients</h3>
             <ul>
                 {recipe.ingredients.map((ingredient, index) => (
